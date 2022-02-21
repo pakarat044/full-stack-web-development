384a4dbe5254b96c57ef5a78c4a04a9128e5cdf5
Phakarat Khongphaisan <Phakarat.kh@ku.th>
2022-02-21 13:38:13 +0000

    Add DELETE endpoint.

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,28 +1,16 @@
 import type { RequestHandler } from "@sveltejs/kit";
+import { api } from "./_api";
 
- // TODO: Persist in database
- let todos: Todo[] = [];
+export const get: RequestHandler = (request) => {
+  return api(request);
+};
 
- export const get: RequestHandler = async ({ request }) => {
-   return {
-     status: 200,
-     body: todos
-   }
- }
-
- export const post: RequestHandler = async ({ request }) => {
-   const data = await request.formData();
-
-   todos.push({
-     created_at: new Date(),
-     text: data.get('text') as string,
-     done: false
-   })
-
-   return {
-     status: 303,
-     headers: {
-       location: "/"
-     }
-   }
- }  
+export const post: RequestHandler = async (request) => {
+  const data = await request.request.formData();
+  return api(request, {
+    uid: `${Date.now()}`, // TODO: Replace the UID based on UUID Package
+    created_at: new Date(),
+    text: data.get("text") as string,
+     done: false,
+   });
+ }; 
